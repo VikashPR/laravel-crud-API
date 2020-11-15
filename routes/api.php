1840ceb77cd650a4d40e4a2b597d7599702aff0b
vikash2806 <vikashrajendran2806@gmail.com>
2020-11-15 19:59:14 +0530

    basic setup finished

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use GuzzleHttp\Promise\Create;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// list Articles
+
+Route::get('articles', 'ArticleController@index');
+
+// List Single Article 
+
+Route::post('article/{id}', 'ArticleController@show');
+
+// Create new Article
+
+Route::put('article', 'ArticleController@store');
+
+// Delete Article
+
+Route::delete('article', 'ArticleController@destroy');
